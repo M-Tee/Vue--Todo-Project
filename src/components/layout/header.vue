2c8868a210ed13baca2c40918befcde0d05fee87
mt <margarettom6@gmail.com>
2020-10-09 15:12:43 +0300

    Add a header

---
+++ src/components/layout/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <h1 class="heading">TodoList</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header"
+}
+</script>
+
+<style scoped>
+.heading {
+  font-size: 1.5em;
+  text-align: center;
+  padding: 20px;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.97);
+  color: #fff;
+}
+
+</style>
